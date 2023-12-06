31e75772c8a10dfb40271480de0da7f511194033
Markus Muschol <markus.muschol@denkwerk.com>
2023-12-06 06:33:28 +0100

    Add day 6

---
+++ src/p6.test.ts
@@ -0,0 +1,29 @@
+import { expect, describe, test as it } from "bun:test";
+import { part1, part2 } from "./p6";
+import { readInputForDay, readInputForDayExample } from "../util";
+
+describe("day 6", () => {
+  describe("part 1", () => {
+    it("example", async () => {
+      const data = await readInputForDayExample(6);
+      expect(part1(data)).toEqual(288);
+    });
+
+    it("input", async () => {
+      const data = await readInputForDay(6);
+      expect(part1(data)).toEqual(71503);
+    });
+  });
+
+  describe("part 2", () => {
+    it("example", async () => {
+      const data = await readInputForDayExample(6);
+      expect(part2(data)).toEqual(46);
+    });
+
+    it("input", async () => {
+      const data = await readInputForDay(6);
+      expect(part2(data)).toEqual(27102791);
+    });
+  });
+});
---
+++ src/p6.ts
@@ -0,0 +1,30 @@
+import { readInputForDay } from "../util";
+
+export const part1 = (input: string[]): number => {
+  const times = input[0].split(":")[1].trim().split(/\s+/).map(Number);
+  const distances = input[1].split(":")[1].trim().split(/\s+/).map(Number);
+  return runRace(times, distances);
+};
+
+export const part2 = (input: string[]): number => {
+  const time = Number(input[0].split(":")[1].trim().split(/\s+/).join(""));
+  const distance = Number(input[1].split(":")[1].trim().split(/\s+/).join(""));
+  return runRace([time], [distance]);
+};
+
+const runRace = (times: number[], distances: number[]) => {
+  return times.reduce((acc, time, index) => {
+    let wins = 0;
+    for (let ms = 1; ms < time; ms++) {
+      const rest = ms * (time - ms);
+      if (rest > distances[index]) {
+        wins++;
+      }
+    }
+    return acc * wins;
+  }, 1);
+};
+
+const data = await readInputForDay(6);
+console.log("Result part 1", part1(data));
+console.log("Result part 2", part2(data));
