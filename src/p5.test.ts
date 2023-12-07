fa4ba243d41c36027c91d9ab492019d569b995f9
Markus Muschol <markus.muschol@denkwerk.com>
2023-12-07 23:34:03 +0100

    Add improved runtime version for day 5

--- src/p5.test.ts
+++ src/p5.test.ts
@@ -1,5 +1,5 @@
 import { expect, describe, test as it } from "bun:test";
-import { lookup, part1, part2 } from "./p5";
+import { part1, part2 } from "./p5";
 import { readInputForDay, readInputForDayExample } from "../util";
 
 describe("day 5", () => {
@@ -13,22 +13,17 @@ describe("day 5", () => {
       const data = await readInputForDay(5);
       expect(part1(data)).toEqual(324724204);
     });
-
-    // it("input", async () => {
-    //   const data = await readInputForDay(4);
-    //   expect(part1(data)).toEqual(25571);
-    // });
   });
 
-  // describe("part 2", () => {
-  //   it("example", async () => {
-  //     const data = await readInputForDayExample(4);
-  //     expect(part2(data)).toEqual(30);
-  //   });
+  describe("part 2", () => {
+    it("example", async () => {
+      const data = await readInputForDayExample(5);
+      expect(part2(data)).toEqual(46);
+    });
 
-  //   it("input", async () => {
-  //     const data = await readInputForDay(4);
-  //     expect(part2(data)).toEqual(8805731);
-  //   });
-  // });
+    it("input", async () => {
+      const data = await readInputForDay(5);
+      expect(part2(data)).toEqual(114257961);
+    });
+  });
 });
--- src/p5.ts
+++ src/p5.ts
@@ -33,24 +33,71 @@ export const part1 = (input: string[]): number => {
   return Math.min(
     ...seeds.map((seed) => {
       return Object.keys(groups).reduce(
-        (acc: number, name) => lookup(groups[name], acc, name),
+        (acc: number, name) => lookupRange(groups[name], acc),
         seed
       );
     })
   );
 };
 
-export function lookup(groupRange: Range[], seed: number, name: string) {
+export const part2 = (input: string[]): number => {
+  const pairs =
+    input[0]
+      .match(/\d+ \d+/g)
+      ?.map((pair) => (pair.split(" ") ?? []).map((p) => Number(p))) ?? [];
+
+  const groups = extractGroups(input);
+  const intersectionPoints = findIntersections(groups);
+
+  const results = pairs.map(([start, length]) => {
+    const arr: number[] = [];
+    for (
+      let i = start;
+      i < start + length;
+      i = intersectionPoints.find((point) => point > i) as number // Find intersection to lower number of possible seeds
+    ) {
+      arr.push(runForSeed(i, groups));
+    }
+    return Math.min(...arr);
+  });
+
+  return Math.min(...results);
+};
+
+const runForSeed = (seed: number, groups: Groups) => {
+  return Object.keys(groups).reduce(
+    (acc: number, name) => lookupRange(groups[name], acc),
+    seed
+  );
+};
+
+function findIntersections(groups: Groups): number[] {
+  return Object.values(groups)
+    .reverse()
+    .reduce((acc, group) => {
+      const sourceList = group.flatMap((x) => [x.source, x.source + 1]);
+      const reverseDestinations = acc.map((dest) => {
+        return lookupRangeReverse(group, dest);
+      });
+      return [...reverseDestinations, ...sourceList];
+    }, [] as number[])
+    .sort();
+}
+
+export function lookupRange(groupRange: Range[], seed: number) {
   const result = groupRange.find((r: Range, i) => {
     return seed >= r.source && seed <= r.source + (r.len - 1);
   });
-  return result ? seed + result.dest - result.source : seed;
+  return result ? seed - result.source + result.dest : seed;
 }
 
-export const part2 = (input: string[]): number => {
-  return 0;
-};
+export function lookupRangeReverse(groupRange: Range[], destination: number) {
+  const result = groupRange.find((r: Range, i) => {
+    return destination >= r.dest && destination <= r.dest + (r.len - 1);
+  });
+  return result ? destination - result.dest + result.source : destination;
+}
 
-const data = await readInputForDay(5);
+const data = await readInputForDayExample(5);
 console.log("Result part 1", part1(data));
-// console.log("Result part 2", part2(data));
+console.log("Result part 2", part2(data));
